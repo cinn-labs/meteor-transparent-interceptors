5d0703891719e268d6d5621fe259b2c6efd10500
Christian Storch <ccstorch@gmail.com>
2016-07-05 15:00:19 +0100

    0.1.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cinn:transparent-interceptors',
-  version: '0.1.0',
+  version: '0.1.1',
   summary: 'Collection smart interceptors',
   git: 'https://github.com/cinn-labs/meteor-transparent-interceptors',
   documentation: 'README.md'
@@ -8,14 +8,14 @@ Package.describe({
 
 Package.onUse(function(api) {
   const both = ['client', 'server'];
-  api.versionsFrom('1.3.2.4');
+  api.versionsFrom('1.3.4');
 
   api.export('TransparentInterceptors');
 
   api.use('ecmascript');
   api.use('meteor-base');
-  api.use('cinn:logged-user@0.0.2');
-  api.use("matb33:collection-hooks@0.7.15");
+  api.use('cinn:logged-user@0.0.3');
+  api.use("matb33:collection-hooks@0.8.1");
 
   api.addFiles('transparent-interceptors.common.js', both);
   api.addFiles('transparent-interceptors.server.js', 'server');
